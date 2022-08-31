a84fed2370c02fd0ce3f6a8afcf1f5ab16b4be64
alessandro <alessandro.assuncao98@gmail.com>
2022-08-31 14:58:20 -0300

    adicionando home, footer, navbar e login

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Footer from './estaticos/footer/Footer';
+import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
+import Navbar from './estaticos/navbar/Navbar';
+
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+
+      <div style={{ minHeight: '100vh' }} >
+
+        <Routes> // Antigo Switch
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/estaticos/footer/Footer.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Grid, Typography } from '@material-ui/core';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { Box } from '@mui/material';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <>
+            <Grid container direction='row' justifyContent='center' alignItems='center'>
+                <Grid alignItems='center' item xs={12}>
+                    <Box className='box1'>
+                        <Box paddingTop={1} display='flex' alignItems='center' justifyContent='center' >
+                            <Typography variant='h5' align='center' gutterBottom className='textos'>
+                                Acompanhe nosso projeto
+                            </Typography>
+                        </Box>
+                        <Box display='flex' alignItems='center' justifyContent='center' >
+                            <a href='https://github.com/projeto56'>
+                                <GitHubIcon className='redes'/>
+                            </a>
+                        </Box>
+                    </Box>
+                    <Box className='box2'>
+                        <Box paddingTop={1}>
+                            <Typography variant='subtitle2' align='center' gutterBottom className='textos'>
+                                &Copy; 2020 Copyright:
+                            </Typography>
+                        </Box>
+                        <Box>
+
+                        </Box>
+
+                    </Box>
+                </Grid>
+            </Grid>
+        </>
+
+    )
+}
+
+export default Footer
