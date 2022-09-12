cca971a3b98754c07933d703df89a369eb19754a
Yasmin <yasmin.nicolau13@gmail.com>
2022-09-12 10:57:35 -0300

    criado cadastrar tema e store

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,43 +1,62 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Box } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Navbar.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
+import { addToken } from '../../../store/tokens/actions';
 
 function Navbar() {
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state)=> state.tokens
+    );
+    let usenavigate= useNavigate();
+    const dispatch = useDispatch();
+
+    function goLogout() {
+        dispatch(addToken(''));
+        alert('Usuario deslogado')
+        usenavigate('/login');
+    }
+    var navbarComponent;
+    if(token != ""){
+        navbarComponent =  <AppBar position="static" className='bg-menu'>
+        <Toolbar variant="dense">
+            <Box display="flex" justifyContent="space-between" className='space'>
+                 
+                    <Box className='cursor'>
+                        <Typography variant="h4">
+                            Blog Informativo
+                        </Typography>
+                    </Box>
+                
+
+                <Box display="flex" justifyContent="space-evenly">
+                    <Link to='/sobrenos' className='text-decorator-none'>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h5" >
+                                Sobre-nós
+                            </Typography>
+                        </Box>
+                    </Link>
+                    <Link to='/login' className='text-decorator-none'>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h5" >
+                                logout
+                            </Typography>
+                        </Box>
+                    </Link>
+
+                </Box>
+            </Box>
+
+        </Toolbar>
+    </AppBar>
+    }
+
     return (
         <>
-            <AppBar position="static" className='bg-menu'>
-                <Toolbar variant="dense">
-                    <Box display="flex" justifyContent="space-between" className='space'>
-                         
-                            <Box className='cursor'>
-                                <Typography variant="h4">
-                                    Blog Informativo
-                                </Typography>
-                            </Box>
-                        
-
-                        <Box display="flex" justifyContent="space-evenly">
-                            <Link to='/sobrenos' className='text-decorator-none'>
-                                <Box mx={1} className='cursor'>
-                                    <Typography variant="h5" >
-                                        Sobre-nós
-                                    </Typography>
-                                </Box>
-                            </Link>
-                            <Link to='/login' className='text-decorator-none'>
-                                <Box mx={1} className='cursor'>
-                                    <Typography variant="h5" >
-                                        logout
-                                    </Typography>
-                                </Box>
-                            </Link>
-
-                        </Box>
-                    </Box>
-
-                </Toolbar>
-            </AppBar>
+             {navbarComponent}
         </>
     )
 }
