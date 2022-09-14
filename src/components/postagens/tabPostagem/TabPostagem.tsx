e4fe9c2c30f65b42efd51abe261ab240217b8333
Vagner <vagner-bw@hotmail.com>
2022-09-14 10:06:19 -0300

    adicionando TabPostagem

---
+++ src/components/postagens/tabPostagem/TabPostagem.tsx
@@ -0,0 +1,42 @@
+import React, {useState} from "react";
+import { AppBar, Tab, Tabs, Typography, Box } from "@mui/material";
+import { TabContext, TabPanel } from "@material-ui/lab";
+import ListaPostagem from "../listaPostagem/ListaPostagens";
+import './TabPostagem.css'
+
+function TabPostagem () {
+    const [value, setValue] = useState('1')
+
+    function handleChange(event: React.ChangeEvent<{}>, newValue: string) {
+      setValue(newValue);  
+    }
+
+    return (
+        <>
+        <TabContext value={value}>
+            <AppBar position="static">
+                <Tabs centered indicatorColor="secondary" onChange={handleChange} >
+                    <Tab label= "Todas as Postagens" value="1"/>
+                    <Tab  label="Sobre Nós" value="2" />
+                </Tabs>
+            </AppBar>
+            <TabPanel value="1">
+                <Box display="flex" flexWrap="wrap" justifyContent="center">
+                    <ListaPostagem />
+                </Box>
+            </TabPanel>
+            <TabPanel value="2">
+                <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className="titulo"> Sobre nós</Typography>
+                <Typography variant="body1" gutterBottom color="textPrimary" align="justify"></Typography>
+            </TabPanel>
+        </TabContext>
+        
+        </>
+
+
+
+    );
+
+}
+
+export default TabPostagem;
